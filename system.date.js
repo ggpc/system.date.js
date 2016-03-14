f4bba605bcb52493a2522126e2d264043bb63d4f
Grigoriy <ggp.work@gmail.com>
2016-03-14 15:26:06 +0200

    Update system.date.js

--- system.date.js
+++ system.date.js
@@ -1,31 +1,4 @@
 (function(module){
-    /**
-        Day     ---     ---
-        d   Day of the month, 2 digits with leading zeros   01 to 31
-        D   A textual representation of a day, three letters    Mon through Sun
-        j   Day of the month without leading zeros  1 to 31
-        l (lowercase 'L')   A full textual representation of the day of the week    Sunday through Saturday
-        N   ISO-8601 numeric representation of the day of the week (added in PHP 5.1.0)     1 (for Monday) through 7 (for Sunday)
-        S   English ordinal suffix for the day of the month, 2 characters   st, nd, rd or th. Works well with j
-        w   Numeric representation of the day of the week   0 (for Sunday) through 6 (for Saturday)
-        Month   ---     ---
-        F   A full textual representation of a month, such as January or March  January through December
-        m   Numeric representation of a month, with leading zeros   01 through 12
-        M   A short textual representation of a month, three letters    Jan through Dec
-        n   Numeric representation of a month, without leading zeros    1 through 12
-        Year    ---     ---
-        Y   A full numeric representation of a year, 4 digits   Examples: 1999 or 2003
-        y   A two digit representation of a year    Examples: 99 or 03
-        Time    ---     ---
-        a   Lowercase Ante meridiem and Post meridiem   am or pm
-        A   Uppercase Ante meridiem and Post meridiem   AM or PM
-        g   12-hour format of an hour without leading zeros     1 through 12
-        G   24-hour format of an hour without leading zeros     0 through 23
-        h   12-hour format of an hour with leading zeros    01 through 12
-        H   24-hour format of an hour with leading zeros    00 through 23
-        i   Minutes with leading zeros  00 to 59
-        s   Seconds, with leading zeros     00 through 59
-    */
     // langs
     var langs = ['en', 'ru', 'ua'];
     var super_default_lang = 'en';
@@ -115,8 +88,8 @@
         var current_cache = {};
         current_cache['Y'] = String(year);
         current_cache['y'] = current_cache['Y'].substr(2);
-        current_cache['s'] = minute>9?String(minute):'0'+String(minute);
-        current_cache['i'] = seconds>9?String(seconds):'0'+String(seconds);
+        current_cache['i'] = minute>9?String(minute):'0'+String(minute);
+        current_cache['s'] = seconds>9?String(seconds):'0'+String(seconds);
         current_cache['a'] = hour<13?'am':'pm';
         current_cache['A'] = hour<13?'AM':'PM';
 
