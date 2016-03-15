4c50fb3aa7cbf5e09a8855f3db42b5d29af213b5
Grigoriy <ggp.work@gmail.com>
2016-03-15 09:28:21 +0200

    Update system.date.js

--- system.date.js
+++ system.date.js
@@ -1,4 +1,27 @@
 (function(module){
+    /**
+        %d   Day of the month, 2 digits with leading zeros   01 to 31
+        %D   A textual representation of a day, three letters    Mon through Sun
+        %j   Day of the month without leading zeros  1 to 31
+        %l (lowercase 'L')   A full textual representation of the day of the week    Sunday through Saturday
+        %N   ISO-8601 numeric representation of the day of the week (added in PHP 5.1.0)     1 (for Monday) through 7 (for Sunday)
+        %S   English ordinal suffix for the day of the month, 2 characters   st, nd, rd or th. Works well with j
+        %w   Numeric representation of the day of the week   0 (for Sunday) through 6 (for Saturday)
+        %F   A full textual representation of a month, such as January or March  January through December
+        %m   Numeric representation of a month, with leading zeros   01 through 12
+        %M   A short textual representation of a month, three letters    Jan through Dec
+        %n   Numeric representation of a month, without leading zeros    1 through 12
+        %Y   A full numeric representation of a year, 4 digits   Examples: 1999 or 2003
+        %y   A two digit representation of a year    Examples: 99 or 03
+        %a   Lowercase Ante meridiem and Post meridiem   am or pm
+        %A   Uppercase Ante meridiem and Post meridiem   AM or PM
+        %g   12-hour format of an hour without leading zeros     1 through 12
+        %G   24-hour format of an hour without leading zeros     0 through 23
+        %h   12-hour format of an hour with leading zeros    01 through 12
+        %H   24-hour format of an hour with leading zeros    00 through 23
+        %i   Minutes with leading zeros  00 to 59
+        %s   Seconds, with leading zeros     00 through 59
+    */
     // langs
     var langs = ['en', 'ru', 'ua'];
     var super_default_lang = 'en';
@@ -164,4 +187,4 @@
     };
     // initialization
     set_date_language(default_lang);
-})(window);
+})(typeof module == 'object'?module:window);
